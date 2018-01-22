79c8b7002188e1fec88d4348bef7275d019c82b6
marat.iskhakov <iskhakov.marat@ordotans.ru>
2018-01-23 00:33:15 +0300

    firebase authorization added

--- app/src/main/java/com/example/firebasedemoapp/LoginActivity.kt
+++ app/src/main/java/com/example/firebasedemoapp/LoginActivity.kt
@@ -1,12 +1,55 @@
 package com.example.firebasedemoapp
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import java.util.*
+import java.util.Arrays.asList
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.firebase.ui.auth.IdpResponse
+import android.content.Intent
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        // Choose authentication providers
+        val providers = Arrays.asList(
+                AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
+                AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build(),
+                AuthUI.IdpConfig.Builder(AuthUI.TWITTER_PROVIDER).build())
+
+// Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // ...
+            } else {
+                // Sign in failed, check response for error code
+                // ...
+            }
+        }
     }
 }
